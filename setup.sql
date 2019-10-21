b2fd0bce3067d0d7343e2031ce6fd59ca044175e
EdoardoErrani993 <edoardo.errani93@gmail.com>
2019-10-21 15:07:33 +0200

    work setup

--- setup.sql
+++ setup.sql
@@ -1,4 +1,4 @@
- --run it from sqlplus as pippo
+--run it from sqlplus as pippo
 --pippo should be created as follow:
 --
 --create user pippo identified by brutti account unlock;
@@ -12,4 +12,20 @@ drop table books;
 --Creation of tables--
 create table books (
     book_id integer primary key,
-    title varchar2(40),
+     title varchar2(40)
+     constraint title_nn not null,
+    --intermediate_id integer
+    --constraint intermediate_id_fk references intermediates(intermediate_id),
+    pages integer
+    constraint pages_nn not null,
+    genre varchar2(40),
+    isbn varchar2(13));
+    
+    create table intermediates (
+     book_id integer
+     constraint book_id_fk references books(book_id),
+     author_id integer
+     constraint author_id_fk references authors(author_id));
+     
+     commit;
+     
