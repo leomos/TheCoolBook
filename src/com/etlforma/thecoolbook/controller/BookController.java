9b5c1193f6f0a8ff2f3a90cee2601fe5afd3719a
PoloRiva <riva.polo@gmail.com>
2019-10-22 15:02:35 +0200

    added some update lines on BookController

--- src/com/etlforma/thecoolbook/controller/BookController.java
+++ src/com/etlforma/thecoolbook/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.etlforma.thecoolbook.model.Book;
@@ -40,5 +41,9 @@ public class BookController {
 		return model;
 	}
 	
+	@RequestMapping(value = "/book/{id}", method=RequestMethod.GET)
+	public void updateBook(@PathVariable int id) {
+		System.out.println("Request book " + id);
+	}
 	
 }
