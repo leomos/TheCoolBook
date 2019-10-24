920575813bc8c9b87bd0a3eb45cb3b91952b001a
PoloRiva <riva.polo@gmail.com>
2019-10-24 09:30:41 +0200

    added the URLimage column to table book and added the values on the insert queries | fixing issue #12

--- setup.sql
+++ setup.sql
@@ -26,16 +26,46 @@ CREATE TABLE book (
     title VARCHAR(255) NOT NULL,
     num_pages INT NOT NULL,
     genre VARCHAR(100) NOT NULL,
-    isbn VARCHAR(13) NOT NULL
+    isbn VARCHAR(13) NOT NULL,
+    URLimage VARCHAR(255)
 );
 
-INSERT INTO book(title, num_pages, genre, isbn) VALUES (
+-- insertion of books
+INSERT INTO book(title, num_pages, genre, isbn, URLimage) VALUES (
 	'Il Signore degli Anelli',
 	500,
 	'Fantasy',
-	'0123456789abc'
+	'0123456789abc',
+    'https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg'
 );
-
+INSERT INTO book(title, num_pages, genre, isbn, URLimage) VALUES (
+	'The importance of being Brutti',
+    '123',
+    'Erotica',
+    '6906660696969',
+    'https://www.nostrofiglio.it/site_stored/imgs/0003/047/armando.630x420.jpg'
+);
+INSERT INTO book(title, num_pages, genre, isbn, URLimage) VALUES (
+	'Electrodynamics',
+    '500',
+    'Physics',
+    '0123333789abc', 
+    'https://upload.wikimedia.org/wikipedia/commons/0/09/Front_cover_of_Griffiths%27_Electrodynamics.jpg'
+);
+INSERT INTO book(title, num_pages, genre, isbn, URLimage) VALUES (
+	'Electrodynamics',
+    '500',
+    'Physics',
+    '0123333789abc', 
+    'https://upload.wikimedia.org/wikipedia/commons/0/09/Front_cover_of_Griffiths%27_Electrodynamics.jpg'
+);
+INSERT INTO book(title, num_pages, genre, isbn, URLimage) VALUES (
+	'Programming with Plankalkül',
+    '267',
+    'Programming',
+    '033789a45234b',
+    'https://bookcover4u.com/pro/Educational-book-cover-design-P1484804032EDB-Java-web-design-internet-business-java.jpg'
+);
 -- author
 DROP TABLE IF EXISTS author;
 
